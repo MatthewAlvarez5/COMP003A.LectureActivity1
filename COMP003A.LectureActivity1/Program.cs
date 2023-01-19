c1226d860e88b1aaaa9cb3c732fc82ef3452d069
matthewalvarez251 <matthewalvarez251@LEM231-31>
2023-01-18 18:56:36 -0800

    Add project files.

---
+++ COMP003A.LectureActivity1/Program.cs
@@ -0,0 +1,19 @@
+﻿// this is a single-line comment
+/* this is a multi-line comment
+ * Author: Matthew Alvarez
+ * Course: COMP003A
+ * Purpose: Lecture Activity for Week 1
+ */
+
+namespace COMP003A.LectureActivity1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("World, Hello!\n");       //classic coding line 
+            Console.WriteLine("this is a string\n");    //this is indeed a string
+            Console.WriteLine("Matthew Alvarez");       
+        }
+    }
+}
